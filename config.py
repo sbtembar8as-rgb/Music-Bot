d2ff2136cda73281474e1e2a76bdb3e9bc750aaf
sbtembar8as-rgb <sbtembar8as@gmail.com>
2026-06-30 01:43:32 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-API_ID = 38841029          # ضع رقم الـ API الخاص بك
-API_HASH = "289dc5614e276b770dfc5718fcd9248d"    # ضع الـ HASH الخاص بك
-BOT_TOKEN = "8709552170:AAE2TBeQe88nCoH5rPemVgYWlOLcYc1qd64"  # توكن البوت من بوت فازر
